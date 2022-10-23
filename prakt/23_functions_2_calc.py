310c62cab64bdd48921a4f98b8594530c0810e59
User <batman45@yandex.ru>
2022-10-23 19:56:35 +0300

    ok

---
+++ prakt/23_functions_2_calc.py
@@ -0,0 +1,26 @@
+# калькулятор
+
+def summ(a, b):
+    return a + b
+def razn(a, b):
+    return a - b
+def umn(a, b):
+    return a * b
+def delen(a, b):
+    return a / b
+
+while True:
+    a = int(input('Первое число: '))
+    c = input('Действие: ')
+    b = int(input('Второе число: '))
+
+    if c == '+':
+        print(summ(a, b))
+    elif c == '-':
+        print(razn(a, b))
+    elif c == '*':
+        print(umn(a, b))
+    elif c == '/':
+        print(delen(a, b))
+    elif c == '0':
+        break
