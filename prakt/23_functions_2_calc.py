3f1a807ec1bc0627be66690bae1a2fffc5a8486f
User <batman45@yandex.ru>
2022-10-24 23:44:46 +0300

    Переписан на лямбду

--- prakt/23_functions_2_calc.py
+++ prakt/23_functions_2_calc.py
@@ -24,3 +24,26 @@ while True:
         print(delen(a, b))
     elif c == '0':
         break
+
+# Практика. Переписать в лямбда выражение
+
+summ = lambda a,b :a + b
+razn = lambda a,b :a - b
+umn = lambda a,b :a * b
+delen = lambda a,b :a / b
+
+while True:
+    a = int(input('Первое число: '))
+    c = input('Действие: ')
+    b = int(input('Второе число: '))
+
+    if c == '+':
+        print(summ(a, b))
+    elif c == '-':
+        print(razn(a, b))
+    elif c == '*':
+        print(umn(a, b))
+    elif c == '/':
+        print(delen(a, b))
+    elif c == '0':
+        break
