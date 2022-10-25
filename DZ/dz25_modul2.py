44c260a7a473bd208fa5856f1fc243d959409c89
User <batman45@yandex.ru>
2022-10-25 21:22:53 +0300

    ok

---
+++ DZ/dz25_modul2.py
@@ -0,0 +1,14 @@
+import dz25_modul1
+
+print(dz25_modul1.summ(3, 5))
+print(dz25_modul1.razn(10, 6))
+print(dz25_modul1.umn(2, 8))
+
+print('____________')
+
+from dz25_modul import one, two
+
+print(one.summ(7, 4))
+print(one.razn(8, 2))
+print(two.umn(3, 12))
+print(two.delen(30, 15))
