b9c88cc5a929c502d7333c4007eeb78fdc8bef3e
User <batman45@yandex.ru>
2022-11-01 21:45:36 +0300

    ok

---
+++ Theory6/dz29_сlasses_animals.py
@@ -0,0 +1,23 @@
+# Создайте класс на тему животных. В классе должен присутствовать конструктор не менее трёх методов.
+
+class Animals:
+    def __init__(self, vid, otryad):
+        self.vid = vid
+        self.otryad = otryad
+        self.dich = ' дикий'
+        self.predator = 'медведь'
+
+    def mlecopitaushie(self):
+        print('Животное ' + self.vid + ' - ' + self.otryad)
+
+    def zoo(self):
+        print(self.vid + self.dich)
+
+    def vseyadn(self):
+        print('А животное ' + self.predator + ' - хищник')
+        print(self.predator + ' тоже ' + self.dich)
+
+zver = Animals('заяц', "травоядное")
+zver.mlecopitaushie()
+zver.zoo()
+zver.vseyadn()
