01be9ee59137359debe90f0782c21db6261207f5
User <batman45@yandex.ru>
2022-11-02 23:44:09 +0300

    ok

---
+++ DZ/dz29-30_file.py
@@ -0,0 +1,18 @@
+from dz30_nasledovanie import Animals, Slon, Tiger, Feesh
+
+zver = Animals('заяц', "травоядное")
+zver.mlecopitaushie()
+zver.zoo()
+zver.vseyadn()
+
+zver2 = Slon('Индийский слон', 'травоядное')
+zver2.mlecopitaushie()
+zver2.Biv()
+
+zver3 = Tiger('Амурский тигр', 'хищное')
+zver3.mlecopitaushie()
+zver3.Meet()
+
+zver4 = Feesh('Лосось', 'ваще-то не млекопитающее')
+zver4.mlecopitaushie()
+zver4.Ikra()
