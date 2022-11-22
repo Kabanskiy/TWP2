6910c3e664d9aecb68029e339dd1b51fd56bfe68
User <batman45@yandex.ru>
2022-11-22 23:44:02 +0300

    добавлена выборка

--- Theory7/37_sqlite3.py
+++ Theory7/37_sqlite3.py
@@ -11,18 +11,34 @@ curs.execute(zapros1) # создаем запрос
 conn.commit() # комитим для сохранени базы
 
 
-# запрос для заполнения данных с таблицу
-zapros2 = """INSERT INTO tablitsa1(login, password) VALUES ('Uassya', '100500');"""
-# чтобы применялось ко всем полям, можно не ставить "(login, password)"
-curs.execute(zapros2)
-conn.commit()
+# # запрос для заполнения данных с таблицу
+# zapros2 = """INSERT INTO tablitsa1(login, password) VALUES ('Uassya', '100500');"""
+# # чтобы применялось ко всем полям, можно не ставить "(login, password)"
+# curs.execute(zapros2)
+# conn.commit()
 
-# также можно добавлять данные через кортеж
-tuple1 = ('Ivan', '2222')
-zapros3 = """INSERT INTO tablitsa1 VALUES (?, ?);""" # на место букв ставим "?"
-curs.execute(zapros3, tuple1) # и ставим название списка или кортежа из которого добавляем
-conn.commit()
+# # также можно добавлять данные через кортеж
+# tuple1 = ('Ivan', '2222')
+# zapros3 = """INSERT INTO tablitsa1 VALUES (?, ?);""" # на место букв ставим "?"
+# curs.execute(zapros3, tuple1) # и ставим название списка или кортежа из которого добавляем
+# conn.commit()
 
+zapros_viborka_dannih = """SELECT * FROM tablitsa1 WHERE login='Ivan'""" # *-значит берем все поля, если не все, то ("название полей")
+# WHERE означает из какого места берем
+curs.execute(zapros_viborka_dannih)
+# чтобы забрать эти данные, нужно использовать метод. Их 3:
+# fetchone
+
+a = curs.fetchone() # забирает первую запись
+print(a)
+
+# fetchmany
+a = curs.fetchmany(2) # в () указываем сколько записей хотим забрать
+print(a)
+
+# fetchall
+a = curs.fetchall() # забирает все записи
+print(a)
 
 curs.close() # после отработки обязательно закрываем методом close
 conn.close() # после отработки обязательно закрываем методом close
