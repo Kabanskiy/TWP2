9ae620c5b0d47bdaae99a35218de199fb26837af
User <batman45@yandex.ru>
2022-11-23 00:25:47 +0300

    ок

--- Theory4/16_matritsi.py
+++ Theory4/16_matritsi.py
@@ -7,15 +7,31 @@
 # for i in range(10):
 #     list2.append([10, 20, 30, 40])
 # print(list2)
+#
+# list3 = []
+# n = int(input('Введи количество строк матрицы: '))
+# m = int(input('Введи количество столбов матрицы: '))
+# for i in range(n):
+#     list3_1 = []
+#     for j in range(m):
+#         a = int(input('Введите элементы: '))
+#         list3_1.append(a)
+#     list3.append(list3_1)
+# for i in list3:                     # вывод таблички
+#     print(i)
 
-list3 = []
+# пользователь вводит числовую матрицу m на n. Найдите сумму каждой строки
+
+list = []
 n = int(input('Введи количество строк матрицы: '))
 m = int(input('Введи количество столбов матрицы: '))
 for i in range(n):
-    list3_1 = []
+    list_2 = []     # матрица должна храниться в списке. Поэтому генерируес строчку, и потом будем добавлять ее в list
     for j in range(m):
-        a = int(input('Введите элементы: '))
-        list3_1.append(a)
-    list3.append(list3_1)
-for i in list3:                     # вывод таблички
+        list_2.append(int(input('Введите элемент матрицы: ')))  # элементы будут добавляться в строчку list_2
+    list.append(list_2)  # будем добавлять строку в общий список
+for i in list:                     # вывод таблички
     print(i)
+
+for i in list:  # находим сумму каждой строки
+    print(sum(i))
